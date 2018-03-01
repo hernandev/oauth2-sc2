4593ab2599f1a9e1d18c3028fc537e0b84bad4ee
Diego Hernandes <diego@hernandev.com>
2018-03-01 15:06:07 -0300

    basic http request (global parser)

---
+++ src/Common/Http/Request.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SteemConnect\OAuth2\Common\Http;
+
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+
+/**
+ * Class Request.
+ *
+ * Implements a singleton around symfony request which is created from global variables.
+ */
+class Request
+{
+    /**
+     * @var Request|null Current request instance.
+     */
+    protected static $instance = null;
+
+    /**
+     * Protected CurrentRequest constructor.
+     */
+    protected function __construct()
+    {
+        //
+    }
+
+    /**
+     * Current request instance retriever methods.
+     *
+     * @return SymfonyRequest
+     */
+    public static function current()
+    {
+        // assign a new instance only if none is present.
+        if (!self::$instance) {
+            self::$instance = SymfonyRequest::createFromGlobals();
+        }
+
+        // return the instance value.
+        return self::$instance;
+    }
+}
